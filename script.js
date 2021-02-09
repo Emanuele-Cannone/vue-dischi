6d80511fcf847a4f5365adc0ef34847ac1ebb64b
Emanuele <cannone.emanuele@gmail.com>
2021-02-09 14:06:01 +0100

    Init

---
+++ script.js
@@ -0,0 +1,11 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+// Stampiamo tutto a schermo, in questo momento non è importante la parte grafica.
+    
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+//     Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+
+// Layout base:
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
+// Togliete $(document).ready(...) dal js
